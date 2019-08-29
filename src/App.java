181ce4758d66030abd7676c88121271012137d5a
roreartes <rominar2008@gmail.com>
2019-08-28 21:33:28 -0300

    Se agrega palabra Inicio en linea 1

---
+++ src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("1) Linea uno. inicio");
+        System.out.println("2) Linea dos");
+        System.out.println("3) Linea tres");
+        System.out.println("4) Linea cuatro");
+        System.out.println("5) Linea cinco");
+    }
+}
